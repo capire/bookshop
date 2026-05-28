8c732ee27021423792b96a6a911adc639f69ce7d
Daniel Hutzel <daniel.hutzel@sap.com>
2026-05-28 16:08:39 +0200

    Leveraging consolidated srv returns

--- srv/cat-service.js
+++ srv/cat-service.js
@@ -14,12 +14,10 @@ class CatalogService extends cds.ApplicationService { init() {
   this.on ('submitOrder', async req => {
     let { book:id, quantity } = req.data
     if (quantity < 1) return req.error (400, `quantity has to be 1 or more`)
-    let succeeded = await UPDATE (Books,id) 
-      .with `stock = stock - ${quantity}` 
-      .where `stock >= ${quantity}` 
-    if (succeeded.affected ?? succeeded) return
-    else if (!this.exists(Books,id)) req.error (404, `Book #${id} doesn't exist`)
-    else req.error (409, `${quantity} exceeds stock for book #${id}`)
+    let {affected} = await UPDATE (Books,id)
+      .with `stock = stock - ${quantity}`
+      .where `stock >= ${quantity}`
+    if (!affected) req.error (409, `${quantity} exceeds stock for book #${id}`)
   })
 
   // Delegate requests to the underlying generic service
