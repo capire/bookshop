6d9205603a48b3d1291c019b41be86d43f52e324
Daniel Hutzel <daniel.hutzel@sap.com>
2026-06-02 08:53:18 +0200

    fix: improve error handling in submitOrder for stock validation

--- srv/cat-service.js
+++ srv/cat-service.js
@@ -17,7 +17,9 @@ class CatalogService extends cds.ApplicationService { init() {
     let {affected} = await UPDATE (Books,id)
       .with `stock = stock - ${quantity}`
       .where `stock >= ${quantity}`
-    if (!affected) req.error (409, `${quantity} exceeds stock for book #${id}`)
+    if (affected) return //> update was successful, so we are done
+    else if (!cds.db.exists(Books,id)) req.error (404, `Book #${id} doesn't exist`)
+    else req.error (409, `${quantity} exceeds stock for book #${id}`)
   })
 
   // Delegate requests to the underlying generic service
